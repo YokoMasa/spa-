0ab695a7a7a830fb97daaf68fff1b21601e01967
YokoMasa <masa.laboratory@gmail.com>
2018-09-13 16:33:31 +0900

    fixed a bug

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,16 +1,16 @@
 class Doctor < ApplicationRecord
-  enum english_speakable: {
-    unknown: 0,
-    yes: 1,
-    no: 2
-  }
-
   enum possibility: {
     unknown: 0,
     willing: 1,
     normal: 2,
     unwilling: 3
-  }
+  }, _prefix: true
+
+  enum english_speakable: {
+    unknown: 0,
+    yes: 1,
+    no: 2
+  }, _prefix: true
 
   belongs_to :department
 
